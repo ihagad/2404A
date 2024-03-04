cdf1ac0b76543853a51c3bef8e9749369ea7f71c
Waffool <137123610+Waffool@users.noreply.github.com>
2024-03-03 19:07:34 -0800

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -2,37 +2,71 @@
 #include "main.h"
 
 // controller
-pros::Controller controller(pros::E_CONTROLLER_MASTER);
+    pros::Controller controller(pros::E_CONTROLLER_MASTER);
+    pros::Motor intake (13, true);
+    pros::Motor shooting (11, true);
+    // pros::ADIDigitalIn limit (20);
+    //pros::Controller controller (CONTROLLER_MASTER);
+    pros::ADIDigitalOut pistonL ('C');
+    pros::ADIDigitalOut pistonR ('B');
+    pros::ADIDigitalOut frontFlap ('A');
 
 // drive motors
-pros::Motor lF(-12, pros::E_MOTOR_GEARSET_06); // left front motor. port 12, reversed
-pros::Motor lM(-16, pros::E_MOTOR_GEARSET_06); // left middle motor. port 11, reversed
-pros::Motor lB(-15, pros::E_MOTOR_GEARSET_06); // left back motor. port 1, reversed
-pros::Motor rF(17, pros::E_MOTOR_GEARSET_06); // right front motor. port 2
-pros::Motor rM(19, pros::E_MOTOR_GEARSET_06); // right middle motor. port 11
-pros::Motor rB(18, pros::E_MOTOR_GEARSET_06); // right back motor. port 13
+    pros::Motor lF(-8, pros::E_MOTOR_GEARSET_06); // left front motor. port 12, reversed
+    pros::Motor lM(-9, pros::E_MOTOR_GEARSET_06); // left middle motor. port 11, reversed
+    pros::Motor lB(-10, pros::E_MOTOR_GEARSET_06); // left back motor. port 1, reversed
+    pros::Motor rF(3, pros::E_MOTOR_GEARSET_06); // right front motor. port 2
+    pros::Motor rM(2, pros::E_MOTOR_GEARSET_06); // right middle motor. port 11
+    pros::Motor rB(1, pros::E_MOTOR_GEARSET_06); // right back motor. port 13
 
 // motor groups
-pros::MotorGroup leftMotors({lF, lM, lB}); // left motor group
-pros::MotorGroup rightMotors({rF, rM, rB}); // right motor group
+    pros::MotorGroup leftMotors({lF, lM, lB}); // left motor group
+    pros::MotorGroup rightMotors({rF, rM, rB}); // right motor group
 
-// Inertial Sensor on port 2
-pros::Imu imu(20);
+// Inertial Sensor on port 20
+    pros::Imu imu(6);
+
+// tracking wheels
+// horizontal tracking wheel encoder. Rotation sensor, port 15, reversed (negative signs don't work due to a pros bug)
+//pros::Rotation horizontalEnc(15, true);
+// horizontal tracking wheel. 2.75" diameter, 3.7" offset, back of the robot (negative)
+//lemlib::TrackingWheel horizontal(&horizontalEnc, lemlib::Omniwheel::NEW_275, -3.7);
 
 // drivetrain settings
 lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
                               &rightMotors, // right motor group
-                              10, // 10 inch track width
+                              11.25, // 10 inch track width
                               lemlib::Omniwheel::NEW_325, // using new 3.25" omnis
                               360, // drivetrain rpm is 360
-                              2 // chase power is 2. If we had traction wheels, it would have been 8
+                              8 // chase power is 2. If we had traction wheels, it would have been 8
 );
 
-// create the chassis
+// lateral motion controller
+lemlib::ControllerSettings linearController(20, // proportional gain (kP)
+                                            0, // integral gain (kI)
+                                            19, // derivative gain (kD)
+                                            3, // anti windup
+                                            1, // small error range, in inches
+                                            100, // small error range timeout, in milliseconds
+                                            3, // large error range, in inches
+                                            500, // large error range timeout, in milliseconds
+                                            20 // maximum acceleration (slew)
+);
 
-//lemlib::Chassis chassis(drivetrain, linearController, angularController, sensors);
+// angular motion controller
+lemlib::ControllerSettings angularController(2, // proportional gain (kP)
+                                             0, // integral gain (kI)
+                                             10, // derivative gain (kD)
+                                             3, // anti windup
+                                             1, // small error range, in degrees
+                                             100, // small error range timeout, in milliseconds
+                                             3, // large error range, in degrees
+                                             500, // large error range timeout, in milliseconds
+                                             0 // maximum acceleration (slew)
+);
 
-
+// sensors for odometry
+// note that in this example we use internal motor encoders (IMEs), so we don't pass vertical tracking wheels
 lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null
                             nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
                             nullptr, // horizontal tracking wheel 1
@@ -40,24 +74,8 @@ lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null
                             &imu // inertial sensor
 );
 
-
-
-
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
+// create the chassis
+lemlib::Chassis chassis(drivetrain, linearController, angularController, sensors);
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -66,71 +84,109 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
+    pros::lcd::initialize(); // initialize brain screen
+    chassis.calibrate(); // calibrate sensors
+    chassis.setPose(0, 0, 0);
 
-	pros::lcd::register_btn1_cb(on_center_button);
+
+    // the default rate is 50. however, if you need to change the rate, you
+    // can do the following.
+    // lemlib::bufferedStdout().setRate(...);
+    // If you use bluetooth or a wired connection, you will want to have a rate of 10ms
+
+    // for more information on how the formatting for the loggers
+    // works, refer to the fmtlib docs
+
+    // thread to for brain screen and position logging
+    pros::Task screenTask([&]() {
+        lemlib::Pose pose(0, 0, 0);
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            // log position telemetry
+            lemlib::telemetrySink()->info("Chassis pose: {}", chassis.getPose());
+            // delay to save resources
+            pros::delay(50);
+        }
+    });
 }
 
 /**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
+ * Runs while the robot is disabled
  */
 void disabled() {}
 
 /**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
+ * runs after initialize if the robot is connected to field control
  */
 void competition_initialize() {}
 
+// get a path used for pure pursuit
+// this needs to be put outside a function
+ASSET(trial1_txt); // '.' replaced with "_" to make c++ happy
 /**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
+ * Runs during auto
  *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
+ * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
  */
-void autonomous() {}
+void autonomous() {
+    chassis.follow(trial1_txt, 2000, 15);
+}
 
 /**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
+ * Runs in driver control
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
+    // controller
+    // loop to continuously update motors
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
+     int time = pros::millis();
 
-		left_mtr = left;
-		right_mtr = right;
+    while (true) {
+        // get joystick positions
+        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+        // move the chassis with curvature drive
 
-		pros::delay(20);
-	}
+        chassis.arcade(leftY, rightX);
+
+        if (controller.get_digital(DIGITAL_L1)){
+        intake.move(127); //outtake
+        } 
+        if (controller.get_digital(DIGITAL_L2)){
+        intake.move(-127); //intake
+        }
+        if(controller. get_digital(DIGITAL_B)){
+        intake.move(0);
+        }
+        //SHOOTING
+        if (controller.get_digital(DIGITAL_R2)){
+        shooting.move(100);
+        } else if (controller.get_digital(DIGITAL_R1)) {
+        shooting.move(0);
+        }
+
+        //FLAPPER
+        if(controller.get_digital(DIGITAL_UP)){
+        frontFlap.set_value(true);
+        }
+        if(controller.get_digital(DIGITAL_DOWN)){
+        frontFlap.set_value(false);
+        }
+
+        //BACK PISTON
+        if(controller.get_digital(DIGITAL_RIGHT)){
+        pistonL.set_value(true);
+        pistonR.set_value(true);
+        }
+        if(controller.get_digital(DIGITAL_LEFT)){
+        pistonL.set_value(false);
+        pistonR.set_value(false);
+        }
+
+        // delay to save resources
+        pros::delay(10);
+    }
 }
